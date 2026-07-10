4eff8f9a5992447c2b5a9a1d34865552d0cc35f6
Paul Stevens <paul.stevens.developer@gmail.com>
2026-07-09 23:07:25 -0700

    feat(client): update ClientEmployeesCard to count only active employees

--- src/components/ClientSummary/ClientEmployeesCard/ClientEmployeesCard.tsx
+++ src/components/ClientSummary/ClientEmployeesCard/ClientEmployeesCard.tsx
@@ -3,14 +3,14 @@ import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
 import "@/components/ClientSummary/ClientEmployeesCard/ClientEmployeesCard.css";
-import type { Employee } from "@/api/generated/models/Employee";
+import { EmployeeStatusEnum, type Employee } from "@/api/generated/models/Employee";
 
 type Props = {
     employees: Employee[];
 };
 
 const ClientEmployeesCard= ({ employees }: Props) => {
-  const employeeCount = employees.length;
+  const employeeCount = employees.filter((employee) => employee.status === EmployeeStatusEnum.Active).length;
 
   return (
     <Card id="client-employees-card">

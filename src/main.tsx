40ed5c4e497f1ef62e00a8e2be2b3d93c8f4b43b
Paul Stevens <paul.stevens.developer@gmail.com>
2026-07-09 20:03:33 -0700

    feat: add CssBaseline component for consistent styling across the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
 import "./index.css";
 import App from "./App.tsx";
@@ -9,6 +10,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
         <App />
       </BrowserRouter>

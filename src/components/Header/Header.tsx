e8c23e199a2f870b3720d4bb5756bc83c3bd5be7
Paul Stevens <paul.stevens.developer@gmail.com>
2026-07-09 20:06:42 -0700

    feat: implement SignInButton component and integrate it into Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import SignInButton from "@/components/Header/SignInButton/SignInButton";
 import "@/components/Header/Header.css";
 
 const Header = () => {
   return (
     <AppBar position="static" id="header">
-      <Toolbar />
+      <Toolbar>
+        <SignInButton />
+      </Toolbar>
     </AppBar>
   );
 };
---
+++ src/components/Header/SignInButton/SignInButton.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import "@/components/Header/SignInButton/SignInButton.css";
+
+const SignInButton = () => {
+  return (
+    <Button color="inherit" id="sign-in-button">
+      Sign In
+    </Button>
+  );
+};
+
+export default SignInButton;

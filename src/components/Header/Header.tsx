3809859bd5f5209580519a5f41d728f86064b0fb
Paul Stevens <paul.stevens.developer@gmail.com>
2026-07-10 23:02:58 -0700

    feat(client): add ClientSummaryButton component to Header for client navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import ClientSelector from '@/components/Header/ClientSelector/ClientSelector';
+import ClientSummaryButton from '@/components/Header/ClientSummaryButton/ClientSummaryButton';
 import SignInButton from '@/components/Header/SignInButton/SignInButton';
 import '@/components/Header/Header.css';
 
@@ -9,6 +10,7 @@ const Header = () => {
     <AppBar position="static" id="header">
       <Toolbar>
         <ClientSelector />
+        <ClientSummaryButton />
         <SignInButton />
       </Toolbar>
     </AppBar>
